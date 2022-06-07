0e9f0e2470e863e761584461432e6cb4c9799b0e
JuliethAnarosa <america02anarosa@gmail.com>
2022-06-07 10:20:36 -0700

    cisfun

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -4,12 +4,14 @@ int main() {
     float floatType;
     double doubleType;
     char charType;
+    long longtype;
 
-    // sizeof evaluates the size of a variable
+    // size of evaluates the size of a variable
     printf("Size of int: %zu bytes\n", sizeof(intType));
     printf("Size of float: %zu bytes\n", sizeof(floatType));
     printf("Size of double: %zu bytes\n", sizeof(doubleType));
     printf("Size of char: %zu byte\n", sizeof(charType));
+    printf("Size of long: %zu byte\n", sizeof(longType));
     
     return 0;
 }

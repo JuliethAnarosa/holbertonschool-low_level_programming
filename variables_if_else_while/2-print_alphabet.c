4562849f79b8d279ff7dc762cdec35063f4bb7b0
JuliethAnarosa <america02anarosa@gmail.com>
2022-06-08 23:15:59 -0700

    putchar

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	char julieth;
+	for (julieth = 'a'; julieth <= 'z'; julieth++)
+		putchar(julieth);
+	return 0;
+}

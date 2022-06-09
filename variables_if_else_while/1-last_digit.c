efcefa07b936f4e173cfec95bd36ae2c7810b0c6
JuliethAnarosa <america02anarosa@gmail.com>
2022-06-08 23:04:48 -0700

    %

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* random number */
 
-/* This program will assign a random number to the variable */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	int n;

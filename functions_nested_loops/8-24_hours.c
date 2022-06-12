989b4e1721be2a4351ef66c7d403047c2fb5e1cf
JuliethAnarosa <america02anarosa@gmail.com>
2022-06-12 06:39:15 -0700

    hour and minute

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,24 +1,24 @@
 #include "main.h"
 /**
  * void jack_bauer - Write a function that prints every minute of the day
- *@n: number being tested
+ *
  * Return: Always 0.
  */
 
 void jack_bauer(void)
 {
-	int hour;
-	int minute;
+	int h;
+	int m;
 
-	for (hour = 0; hour < 24; hour++)
+	for (h = 0; h < 24; h++)
 	{
-		for (minute = 0; minute <60; minute++)
+		for (m = 0; m <60; m++)
 			{
-			_putchar((hr / 10) + '0');
-			_putchar((hr % 10) + '0');
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
 			_putchar(':');
-			_putchar((min / 10) + '0');
-			_putchar((min % 10) + '0');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
 			_putchar('\n');
 			}
 	}

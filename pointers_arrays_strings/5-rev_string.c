3169414c6d69e3ea9d96c595db714a84e1c4abc3
JuliethAnarosa <america02anarosa@gmail.com>
2022-06-16 23:07:23 -0500

    rev

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - Write a function that reverses a string.
+ *@s: string to print
+ * Return:void
+ */
+
+void rev_string(char *s)
+{
+	int i = 0, j = 0;
+	char str[100];
+
+	while (*(s + i))
+	{
+		*(str + i) = *(s + i);
+		i++;
+	}
+	i = i - 1;
+	while (i >= 0)
+	{
+		*(s + i) = *(str + j);
+		j++;
+		i--;
+	}
+}

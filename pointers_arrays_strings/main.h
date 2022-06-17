38ea965d79ee0bec2d4185286a23b489bafb9834
JuliethAnarosa <america02anarosa@gmail.com>
2022-06-16 22:51:59 -0500

    reverse

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,15 @@
 #ifndef main_h
 #define main_h
 
+/**
+ * File: main.h
+ * 
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x04-pointers_arrays_strings directory.
+ */
+
+#include <stdlib.h>
+
 void reset_to_98(int *n);
 int _putchar(char c);
 void swap_int(int *a, int *b);

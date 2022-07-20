71d03bdad56906245322c7e83c7d2c4713f0a290
JuliethAnarosa <america02anarosa@gmail.com>
2022-07-20 15:42:47 -0500

    fix: spaces at the start of a line

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -2,7 +2,7 @@
 #include <stddef.h>
 /**
  * binary_to_uint - Converts a binary number to an unsigned int.
- *mult: 
+ * mult: 
  * @b: A pointer to a string of 0 and 1 chars.
  * Return: the converted number, or 0 if
  * there is one or more chars in the string b that is not 0 or 1
@@ -11,22 +11,18 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int num = 0, mult = 1;
-    int len;
-    if (b == NULL)
-	    return (0);
-
-    for (len = 0; b[len];)
-        len++;
-
-    for (len = len - 1; len >= 0; len--)
-    {
-        if (b[len] != '0' && b[len] != '1')
-            return (0);
-
-        num = num + (b[len] - '0') * mult;
-        mult = mult * 2;
-    }
-
-    return (num);
+	unsigned int num = 0, mult = 1;
+	int len;
+	if (b == NULL)
+		return (0);
+	for (len = 0; b[len];)
+		len++;
+	for (len = len - 1; len >= 0; len--)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+		num = num + (b[len] - '0') * mult;
+		mult = mult * 2;
+	}
+	return (num);
 }
